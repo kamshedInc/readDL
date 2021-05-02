c96f4a86518c0711109a4066d0f9a708dac7ecc6
Joshrlear <joshrlear@gmail.com>
2021-05-02 13:48:11 -0700

    init

---
+++ index.js
@@ -0,0 +1,71 @@
+module.exports = async function (context, req) {
+    if (!req.body) context.res.status(400).send("Please provide image.")
+
+    const fetch = require('node-fetch')
+    const controller = new AbortController()
+    const url = process.env.READAPI_READ
+    const keyname = process.env.READAPI_KEY_NAME
+    const key = process.env.READAPI_KEY_VALUE
+    
+
+    // payload
+    const opt = {
+        method: 'POST',
+        body: blob,
+        headers: {
+           'Content-Type': 'application/octet-stream', 
+           [keyname]: key
+        },
+        signal: controller.signal
+    }
+
+
+
+
+    //  send to read
+    //  =======================================================
+    const readDL = new Promise(res => {
+        fetch(url, opt)
+        .then(d => {
+            if (d.headers.has('Operation-Location')) {
+                controller.abort()
+                res(d.headers.get('Operation-Location'))
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    })
+
+
+
+
+
+
+    //  get results from read
+    //  =======================================================
+    readDL.then(getRead => {
+        new Promise(res => checkResult(getRead,keyname,key,res))
+        .then(r => {
+            let result;
+            new Promise((res,rej) => getInfo(r,res,rej))
+            .then(r => {
+                result = r
+                res(r)
+            }).catch(() => {
+                console.log("Not enough lines. this is the result")
+                return result
+            })
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+
+    context.res = { body: data }
+}
+
+
+
+export function readLicense(blob,res) {
+    
+}
